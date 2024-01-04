03ee28767a69d9715734319f503ec9d880d0870c
Hamada-hadi <ahmedgazow@gmail.com>
2024-01-03 21:24:07 -0700

    First commit

---
+++ module.py
@@ -0,0 +1,10 @@
+
+import math
+
+def calculate_square_footage(length, width):
+    area = length * width
+    return area
+
+def calculate_circumference(radius):
+    circumference = 2 * math.pi * radius
+    return circumference
